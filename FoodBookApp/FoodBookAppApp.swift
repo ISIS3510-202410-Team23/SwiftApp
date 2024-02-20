4ecab3e3ba60e004a678c8abcc12d84aa6e6505e
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2024-02-20 10:23:03 -0500

    Inital commit

---
+++ FoodBookApp/FoodBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodBookAppApp.swift
+//  FoodBookApp
+//
+//  Created by Maria Castro on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
