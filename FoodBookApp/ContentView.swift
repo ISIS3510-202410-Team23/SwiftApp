0e373defe371b916e2bf7da166ab32361e24b254
juanyepesp <j.yepes@uniandes.edu.co>
2024-02-20 10:40:43 -0500

    Added a circle!

--- FoodBookApp/ContentView.swift
+++ FoodBookApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hola, mundo!")
+            Circle().stroke(Color.blue)
         }
         .padding()
     }
