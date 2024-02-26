d13cf50bca3b6c200e2fdbe9864ec5ba08f92e94
juanyepesp <j.yepes@uniandes.edu.co>
2024-02-25 22:07:35 -0500

    feat: Created respective components
    New components added (just views, no functionality):
     - `SpotCard`: Creates a card for the spot
     - `HorCategoryList`:  Lists some categories, is not very responsive or scrollable atm.
    
    Also the browse view shows usage of the components.

--- FoodBookApp/UI/Views/BrowseView.swift
+++ FoodBookApp/UI/Views/BrowseView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct BrowseView: View {
+    // FIXME: maybe all of these parameters should come from the API?
+    struct Spot: Hashable {
+        let name: String
+        let minTime: Int
+        let maxTime: Int
+        let distance: Double
+        let categories: [String]
+    }
+       
+    
+    let spots: [Spot] = [
+        Spot(
+            name: "Mi Caserito",
+            minTime: 10,
+            maxTime: 20,
+            distance: 0.3,
+            categories: ["Fast", "Homemade", "Colombian", "..."]
+        ),
+        Spot(
+            name: "Divino Pecado",
+            minTime: 25,
+            maxTime: 30,
+            distance: 0.5,
+            categories: ["Vegan", "Sandwich", "Bowl", "Healthy", "..."]
+        )
+    ]
+    
     var body: some View {
-        ZStack {
-            // FIXME: the following content is temporary
-            Image(systemName: "magnifyingglass.circle")
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .font(.system(size: 100))
-        }
+        // TODO: Missing searchbar for spots
+        // TODO: Missing filter for categories
+        ScrollView(content: {
+            ForEach(spots, id: \.self){
+                spot in
+                SpotCard(
+                    title: spot.name,
+                    minTime: spot.minTime,
+                    maxTime: spot.maxTime,
+                    distance: Float(spot.distance),
+                    categories: spot.categories
+                )
+                .fixedSize(horizontal: false, vertical: true)
+            }
+        }).padding(8)
     }
 }
 
