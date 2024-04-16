3d87480cd19aeb2f241d90d4b0eaf18e7662b8f0
juanyepesp <j.yepes@uniandes.edu.co>
2024-04-16 14:25:14 -0500

    refactor: added a new ViewModel

---
+++ FoodBookApp/UI/Views/Content/ContentViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ContentViewModel.swift
+//  FoodBookApp
+//
+//  Created by Juan Diego Yepes Parra on 16/04/24.
+//
+
+import Foundation
+import CoreData
+import FirebaseFirestore
+
+@Observable
+class ContentViewModel {
+    @Published var browseSpots: [Spot] = []
+    @Published var forYouSpots: [Spot] = []
+    
+    private let locationService = LocationService.shared
+    private let locationUtils = LocationUtils()
+    private let repository: SpotRepository = SpotRepositoryImpl.shared
+    private let backendService = BackendService()
+    private let utils = Utils.shared
+    
+//    private let cache = NSCache<NSString, Spot>()
+    
+    func fetch() async throws {
+        
+    }
+}
