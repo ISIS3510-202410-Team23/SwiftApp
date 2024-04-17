4a096280cdea761570b00e97bcc6e242dc13d22a
juanyepesp <j.yepes@uniandes.edu.co>
2024-04-17 13:35:27 -0500

    fix: added usage of networkService for handling crashes
    note: this is just a temporary fix!

--- FoodBookApp/UI/Views/Browse/BrowseView.swift
+++ FoodBookApp/UI/Views/Browse/BrowseView.swift
@@ -8,44 +8,61 @@
 import SwiftUI
 
 struct BrowseView: View {
-    @State private var model = BrowseViewModel()
+    
     @Binding var searchText: String
-    @State private var isFetching = false // Track fetching status
+    @State private var model = BrowseViewModel()
+    @State private var isFetching = false
+    @State private var showAlert = false
+    @ObservedObject var networkService = NetworkService.shared
     
     var body: some View {
         ScrollView {
             VStack {
-                if !isFetching && searchResults.isEmpty {
-                    Text("Hmm, nothing here. Maybe try a different search?")
+                if networkService.isOnline {
+                    if !isFetching && searchResults.isEmpty {
+                        Text("Hmm, nothing here. Maybe try a different search?")
+                            .foregroundColor(.secondary)
+                            .multilineTextAlignment(.center)
+                            .safeAreaPadding()
+                    } else {
+                        ForEach(searchResults, id: \.self) { spot in
+                            NavigationLink(destination: SpotDetailView(spotId: spot.id ?? "")){
+                                SpotCard(
+                                    title: spot.name,
+                                    minTime: spot.waitTime.min,
+                                    maxTime: spot.waitTime.max,
+                                    distance: spot.distance ?? "-",
+                                    categories: Array(Utils.shared.highestCategories(spot: spot).prefix(5)),
+                                    imageLinks: spot.imageLinks ?? [],
+                                    price: spot.price
+                                )
+                                .fixedSize(horizontal: false, vertical: true)
+                                .accentColor(.black)
+                            }
+                        }
+                    }
+                    
+                    if isFetching {
+                        ProgressView()
+                            .padding()
+                    }
+                } else {
+                    Text("Nothing here... yet!")
                         .foregroundColor(.secondary)
                         .multilineTextAlignment(.center)
                         .safeAreaPadding()
-                } else {
-                    ForEach(searchResults, id: \.self) { spot in
-                        NavigationLink(destination: SpotDetailView(spotId: spot.id ?? "")){
-                            SpotCard(
-                                title: spot.name,
-                                minTime: spot.waitTime.min,
-                                maxTime: spot.waitTime.max,
-                                distance: spot.distance ?? "-",
-                                categories: Array(Utils.shared.highestCategories(spot: spot).prefix(5)),
-                                imageLinks: spot.imageLinks ?? [],
-                                price: spot.price
-                            )
-                            .fixedSize(horizontal: false, vertical: true)
-                            .accentColor(.black)
-                        }
-                    }
-                }
-                
-                // Progress View
-                if isFetching {
-                    ProgressView()
-                        .padding()
                 }
             }
         }
         .padding(8)
+        .alert("Please check your internet connection and try again", isPresented: $showAlert) {
+            Button("OK", role: .cancel) { }
+        }
+        .onReceive(networkService.$isOnline) { isOnline in
+            if !isOnline {
+                showAlert = true
+            }
+        }
         .task {
             isFetching = true
             _ = try? await model.fetchSpotsAndCalculateDistance()
--- FoodBookApp/UI/Views/ForYou/ForYouView.swift
+++ FoodBookApp/UI/Views/ForYou/ForYouView.swift
@@ -9,42 +9,61 @@ import SwiftUI
 
 
 struct ForYouView: View {
+    
     @State private var model = ForYouViewModel()
     @State private var isFetching = true
     @State private var showNotFoundError = false
+    @State private var showAlert = false
+    @ObservedObject var networkService = NetworkService.shared
+    
     
     var body: some View {
         ScrollView {
             Group {
-                if isFetching {
-                    ProgressView()
-                } else if showNotFoundError {
-                    Text("Leave reviews to get personalized recommendations!")
+                if networkService.isOnline {
+                    if isFetching {
+                        ProgressView()
+                    } else if showNotFoundError {
+                        Text("Leave reviews to get personalized recommendations!")
+                            .foregroundColor(.secondary)
+                            .multilineTextAlignment(.center)
+                            .safeAreaPadding()
+                    } else {
+                        ForEach(model.spots, id: \.self) { spot in
+                            NavigationLink(
+                                destination: SpotDetailView(spotId: spot.id ?? ""))
+                            {
+                                SpotCard(
+                                    title: spot.name,
+                                    minTime: spot.waitTime.min,
+                                    maxTime: spot.waitTime.max,
+                                    distance: spot.distance ?? "-",
+                                    categories: spot.categories,
+                                    imageLinks: spot.imageLinks ?? [],
+                                    price: spot.price
+                                )
+                                .fixedSize(horizontal: false, vertical: true)
+                            }
+                            .accentColor(.black)
+                        }
+                    }
+                } else {
+                    Text("Nothing here... yet!")
                         .foregroundColor(.secondary)
                         .multilineTextAlignment(.center)
                         .safeAreaPadding()
-                } else {
-                    ForEach(model.spots, id: \.self) { spot in
-                        NavigationLink(
-                            destination: SpotDetailView(spotId: spot.id ?? ""))
-                        {
-                            SpotCard(
-                                title: spot.name,
-                                minTime: spot.waitTime.min,
-                                maxTime: spot.waitTime.max,
-                                distance: spot.distance ?? "-",
-                                categories: spot.categories,
-                                imageLinks: spot.imageLinks ?? [],
-                                price: spot.price
-                            )
-                            .fixedSize(horizontal: false, vertical: true)
-                        }
-                        .accentColor(.black)
-                    }
                 }
             }
         }
         .padding(8)
+        .alert("Please check your internet connection and try again", isPresented: $showAlert) {
+            Button("OK", role: .cancel) { }
+        }
+        .onReceive(networkService.$isOnline) { isOnline in
+            if !isOnline {
+                showAlert = true
+            }
+        }
         .task {
             do {
                 isFetching = true
