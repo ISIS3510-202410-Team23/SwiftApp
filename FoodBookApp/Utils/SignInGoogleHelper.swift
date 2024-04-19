cfa38deac2457b187f3320bccfc5fd3b2f37fd7f
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2024-04-19 12:44:05 -0500

    wip(): timeout for sigin browser
    still missing trigger so that alert is shown to the usre
    
    Signed-off-by: Maria Castro Iregui <m.castroi@uniandes.edu.co>

--- FoodBookApp/Utils/SignInGoogleHelper.swift
+++ FoodBookApp/Utils/SignInGoogleHelper.swift
@@ -23,18 +23,54 @@ final class SignInGoogleHelper {
             throw URLError(.cannotFindHost)
         }
         
-        let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
-        
-        guard let idToken = gidSignInResult.user.idToken?.tokenString else {
-            throw URLError(.badServerResponse)
+        do {
+            let gidSignInResult = try await self.signInWithTimeout(withPresenting: topVC)
+            print("result \(gidSignInResult)")
+            guard let idToken = gidSignInResult.user.idToken?.tokenString else {
+                throw URLError(.badServerResponse)
+            }
+            
+            let accessToken = gidSignInResult.user.accessToken.tokenString
+            let name = gidSignInResult.user.profile?.name
+            let email = gidSignInResult.user.profile?.email
+            
+            let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken, name: name, email: email)
+            return tokens
+        } catch {
+            print("err: ", error)
+            throw error
         }
         
-        let accessToken = gidSignInResult.user.accessToken.tokenString
-        let name = gidSignInResult.user.profile?.name
-        let email = gidSignInResult.user.profile?.email
-        
-        let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken, name: name, email: email)
-        return tokens
     }
     
+    func signInWithTimeout(withPresenting topVC: UIViewController) async throws -> GIDSignInResult {
+        
+        let signInTask = Task { @MainActor  in
+            try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        }
+        
+        let timeoutTask = Task (priority: .background) {
+//            print("start timeout")
+            try await Task.sleep(for: .seconds(10)) // User has 30 seconds to complete login process
+//            print("end timeout")
+            await topVC.dismiss(animated: true)
+            try Task.checkCancellation()
+            print(signInTask.isCancelled)
+            signInTask.cancel()
+            print(signInTask.isCancelled)
+            signInTask.cancel()
+            //
+            throw CancellationError()
+        }
+        
+        return try await withTaskCancellationHandler {
+            print("operation start")
+            let result = try await signInTask.value
+            //            timeoutTask.cancel()
+            return result
+            
+        } onCancel: {
+            print("oncancele")
+        }
+    }
 }
